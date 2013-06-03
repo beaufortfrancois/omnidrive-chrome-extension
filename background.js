2c8a6ccce807c514c0aaaaf51c86d1e98341545c
François Beaufort <beaufort.francois@gmail.com>
2013-06-03 11:03:35 +0200

    Switched to chrome.identity and removed chrome.tabs.getSelected

--- background.js
+++ background.js
@@ -1,30 +1,8 @@
 /**
- * OAuth stuff are easy thanks to smus.com/oauth2-chrome-extensions 
- */
-
-var google = new OAuth2('google', {
-    client_id: '652497263904.apps.googleusercontent.com',
-    client_secret: 'ldieNQaIOqmNnyat6PZR4cBe',
-    api_scope: 'https://www.googleapis.com/auth/drive.readonly'
-});
-
-
-/**
- * Oauth webflow starts when user starts to search in OmniDrive 
- */
-
-chrome.omnibox.onInputStarted.addListener(function () {
-    if (google.getAccessToken() === undefined || google.isAccessTokenExpired()) {
-        google.authorize();
-    }
-});
-
-
-/**
  * We keep track of the request object in order to cancel it easily
  */
 
-var currentRequest = null;
+var xhr = null;
 
 
 /**
@@ -34,16 +12,11 @@ var currentRequest = null;
  */
 
 chrome.omnibox.onInputEntered.addListener(function (text) {
-    chrome.tabs.getSelected(null, function (tab) {
-        if (/^https:\/\/docs.google.com\//.test(text)) {
-            url = text
-        } else {
-            url = "https://drive.google.com/#search/" + text
-        }
-        chrome.tabs.update(tab.id, {
-            url: url
-        });
-    });
+    if (/^https:\/\/docs.google.com\//.test(text)) {
+        chrome.tabs.update({ url: text });
+    } else {
+        chrome.tabs.update({ url: 'https://drive.google.com/#search/' + text });
+    }
 });
 
 
@@ -52,37 +25,32 @@ chrome.omnibox.onInputEntered.addListener(function (text) {
  */
 
 chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
-    if (currentRequest != null) {
-        currentRequest.onreadystatechange = null;
-        currentRequest.abort();
-        currentRequest = null;
-    }
-
-    if (text == '') return;
-
-    currentRequest = search(text, function (response) {
+    search(text, function (response) {
         var results = [];
-
-        response = JSON.parse(response);
-        response.items.forEach(function (item) {
+        JSON.parse(response).items.forEach(function (item) {
             results.push({
                 content: item.alternateLink,
-                description: item.title + " <url>" + item.alternateLink + "</url> "
+                description: item.title
             });
-            suggest(results);
-        })
+        });
+        suggest(results);
     });
 });
 
 
 function search(query, callback) {
-    var url = "https://www.googleapis.com/drive/v2/files?maxResults=5&fields=items(alternateLink%2Ctitle)&q=fullText+contains+'" + query + "'";
+    var url = 'https://www.googleapis.com/drive/v2/files?maxResults=5&fields=items(alternateLink%2Ctitle)&q=fullText+contains+"' + query + '"';
 
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.setRequestHeader('Authorization', 'OAuth ' + google.getAccessToken())
-    xhr.onload = function () { callback(xhr.response); }
-    xhr.send();
-    return xhr;
+    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+        if (xhr !== null) {
+            xhr.onreadystatechange = null;
+            xhr.abort();
+            xhr = null;
+        }
+        xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        xhr.onload = function () { callback(xhr.response); }
+        xhr.send(null);
+    });
 }
-
