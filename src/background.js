296b3c99c248023afa958967b2659eac3f35d7be
Francois Beaufort <beaufort.francois@gmail.com>
2015-06-08 11:59:52 +0200

    Added Log in message when user is not logged in yet

--- src/background.js
+++ src/background.js
@@ -1,18 +1,21 @@
+const LOGIN_TEXT = 'Click here to authenticate...';
+
 /**
- * We keep track of the request object in order to cancel it easily
+ * We keep track of the request object in order to cancel it easily.
  */
 
 var xhr = null;
 
 
 /**
- * If user hits <Enter> key, open a tab to Google Drive Web Search if 
- * text starts with http or directly to the Google Drive item if user 
- * selected it   
+ * If user hits <Enter> key, open a tab to Google Drive Web Search if text
+ * starts with Drive Domain or to the Google Drive item if user selected it.
  */
 
 chrome.omnibox.onInputEntered.addListener(function (text) {
-    if (/^https:\/\/docs.google.com\//.test(text)) {
+    if (text == LOGIN_TEXT) {
+        chrome.identity.getAuthToken({ 'interactive': true }, function() {});
+    } else if (/^https:\/\/docs.google.com\//.test(text)) {
         chrome.tabs.update({ url: text });
     } else {
         chrome.tabs.update({ url: 'https://drive.google.com/#search/' + text });
@@ -21,36 +24,47 @@ chrome.omnibox.onInputEntered.addListener(function (text) {
 
 
 /**
- * Ask Google Drive API Search when user enters something in the omnibox
+ * Ask Google Drive API Search when user enters something in the omnibox.
  */
 
-chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
-    search(text, function (response) {
-        var results = [];
-        JSON.parse(response).items.forEach(function (item) {
-            results.push({
-                content: item.alternateLink,
-                description: item.title
-            });
-        });
-        suggest(results);
-    });
-});
+chrome.omnibox.onInputChanged.addListener(onInputChanged);
 
+function onInputChanged(text, suggest) {
+    var url = 'https://www.googleapis.com/drive/v2/files?' +
+              'maxResults=5&fields=items(alternateLink%2Ctitle)&' +
+              'q=fullText+contains+"' + text + '"';
 
-function search(query, callback) {
-    var url = 'https://www.googleapis.com/drive/v2/files?maxResults=5&fields=items(alternateLink%2Ctitle)&q=fullText+contains+"' + query + '"';
+    if (xhr !== null) {
+        xhr.onreadystatechange = null;
+        xhr.abort();
+        xhr = null;
+    }
 
-    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-        if (xhr !== null) {
-            xhr.onreadystatechange = null;
-            xhr.abort();
-            xhr = null;
+    chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
+        if (chrome.runtime.lastError || !token) {
+            suggest([{content: LOGIN_TEXT, description: LOGIN_TEXT}]);
+            return;
         }
         xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
+        xhr.open('GET', url);
         xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-        xhr.onload = function () { callback(xhr.response); }
-        xhr.send(null);
+        xhr.responseType = 'json';
+        xhr.onloadend = function () {
+            if (xhr.status == 200) {
+                var results = [];
+                xhr.response.items.forEach(function(item) {
+                    results.push({
+                        content: item.alternateLink,
+                        description: item.title
+                    });
+                });
+                suggest(results);
+            } else if (xhr.status == 401) {
+                chrome.identity.removeCachedAuthToken({ token: token }, function() {
+                    onInputChanged(text, suggest);
+                });
+            }
+        }
+        xhr.send();
     });
-}
+};
